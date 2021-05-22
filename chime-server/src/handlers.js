51d5dec78e8018f2fc63c07015c63782a6180761
Bjoern Heinrichs <bjoern@heinrichs.pro>
2021-05-22 10:57:34 +0000

    implement deployment

--- chime-server/src/handlers.js
+++ chime-server/src/handlers.js
@@ -33,65 +33,7 @@ exports.index = async (event, context, callback) => {
   return response(200, 'text/html', fs.readFileSync('./index.html', {encoding: 'utf8'}));
 };
 
-exports.join = async(event, context) => {
-  const query = event.queryStringParameters;
-  if (!query.title || !query.name || !query.region) {
-    return response(400, 'application/json', JSON.stringify({error: 'Need parameters: title, name, region'}));
-  }
 
-  // Look up the meeting by its title. If it does not exist, create the meeting.
-  let meeting = await getMeeting(query.title);
-  if (!meeting) {
-    const request = {
-      // Use a UUID for the client request token to ensure that any request retries
-      // do not create multiple meetings.
-      ClientRequestToken: uuidv4(),
-
-      // Specify the media region (where the meeting is hosted).
-      // In this case, we use the region selected by the user.
-      MediaRegion: query.region,
-
-      // Set up SQS notifications if being used
-      NotificationsConfiguration: USE_EVENT_BRIDGE === 'false' ? { SqsQueueArn: SQS_QUEUE_ARN } : {},
-
-      // Any meeting ID you wish to associate with the meeting.
-      // For simplicity here, we use the meeting title.
-      ExternalMeetingId: query.title.substring(0, 64),
-
-      // Tags associated with the meeting. They can be used in cost allocation console
-      Tags: [
-        { Key: 'Department', Value: 'RND'}
-      ]
-    };
-    console.info('Creating new meeting: ' + JSON.stringify(request));
-    meeting = await chime.createMeeting(request).promise();
-
-    // Store the meeting in the table using the meeting title as the key.
-    await putMeeting(query.title, meeting);
-  }
-
-  // Create new attendee for the meeting
-  console.info('Adding new attendee');
-  const attendee = (await chime.createAttendee({
-    // The meeting ID of the created meeting to add the attendee to
-    MeetingId: meeting.Meeting.MeetingId,
-
-    // Any user ID you wish to associate with the attendeee.
-    // For simplicity here, we use a random UUID for uniqueness
-    // combined with the name the user provided, which can later
-    // be used to help build the roster.
-    ExternalUserId: `${uuidv4().substring(0, 8)}#${query.name}`.substring(0, 64),
-  }).promise());
-
-  // Return the meeting and attendee responses. The client will use these
-  // to join the meeting.
-  return response(200, 'application/json', JSON.stringify({
-    JoinInfo: {
-      Meeting: meeting,
-      Attendee: attendee,
-    },
-  }, null, 2));
-};
 
 exports.end = async (event, context) => {
   // Fetch the meeting by title
