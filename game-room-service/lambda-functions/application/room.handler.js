cfec1d471eaa6b30787ec33ba7802e7b60bc8707
mayjunjuly0313 <tank6138@gmail.com>
2021-05-22 21:31:11 +0900

    feat: game room lambda functions

---
+++ game-room-service/lambda-functions/application/room.handler.js
@@ -0,0 +1,136 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB();
+const chime = new AWS.Chime({ region: 'us-east-1' });
+const { v4: uuidv4 } = require('uuid');
+chime.endpoint = new AWS.Endpoint(process.env.CHIME_ENDPOINT);
+const {
+  getGameRoom,
+  putGameRoom,
+  deleteGameRoom,
+} = require('/infrastucture/dynamo.repository.js');
+
+const response = {
+  statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS,GET,PUT,POST',
+    'Content-Type': 'application/json',
+  },
+  body: '',
+  isBase64Encoded: false,
+};
+
+exports.join = async (event, context, callback) => {
+  const query = event.queryStringParameters;
+  if (!query.roomId) {
+    response.statusCode = 400;
+    response.body = 'Need parameters: roomId';
+    callback(null, response);
+    return;
+  }
+
+  let gameRoom = await getGameRoom(query.roomId);
+  if (!gameRoom) {
+    response.statusCode = 404;
+    response.body = 'Invalid roomId';
+    callback(null, response);
+    return;
+  }
+
+  // Create new player for the meeting
+  console.info('Adding new player');
+  const player = await chime
+    .createAttendee({
+      MeetingId: gameRoom.Meeting.MeetingId,
+      ExternalUserId: `${uuidv4().substring(0, 8)}#${query.name}`.substring(
+        0,
+        64
+      ),
+    })
+    .promise();
+
+  response.statusCode = 200;
+  response.body = {
+    JoinInfo: {
+      gameRoom: gameRoom,
+      player: player,
+    },
+  };
+  callback(null, response);
+  return;
+};
+
+exports.createGameRoom = async (event, context, callback) => {
+  let roomId = uuidv4();
+  // Look up the meeting by its title. If it does not exist, create the meeting.
+  let gameRoom = await getGameRoom(roomId);
+  if (gameRoom) {
+    response.statusCode = 404;
+    response.body = 'room already exist, try again';
+    callback(null, response);
+    return;
+  }
+
+  const request = {
+    ClientRequestToken: uuidv4(),
+    ExternalMeetingId: roomId.substring(0, 64),
+  };
+  console.info('Creating new game room: ' + JSON.stringify(request));
+  const room = await chime.createMeeting(request).promise();
+  await putGameRoom(roomId, room);
+
+  console.info('Adding host player');
+  const player = await chime
+    .createAttendee({
+      MeetingId: room.Meeting.MeetingId,
+      ExternalUserId: uuidv4(),
+    })
+    .promise();
+
+  const joinInfo = {
+    JoinInfo: {
+      hostInfo: player.Attendee,
+      roomInfo: room.Meeting,
+    },
+  };
+  console.info('joinInfo:', JSON.stringify(joinInfo, null, 2));
+  response.statusCode = 200;
+  response.body = JSON.stringify(joinInfo, '', 2);
+  callback(null, response);
+};
+
+exports.endRoom = async (event, context, callback) => {
+  console.log('end game:', JSON.stringify(event, null, 2));
+  const query = event.queryStringParameters;
+
+  if (!query.roomId) {
+    console.log('end event > missing required fields: provide roomId');
+    response.statusCode = 400;
+    response.body = 'Must provide roomId';
+    callback(null, response);
+    return;
+  }
+
+  response.statusCode = 200;
+  response.body = JSON.stringify(endGameRoom(query.roomId));
+  console.info('end Room > response:', JSON.stringify(response, null, 2));
+  callback(null, response);
+};
+
+const endGameRoom = async (roomId) => {
+  const roomInfo = await getGameRoom(roomId);
+
+  try {
+    await chime
+      .deleteMeeting({
+        MeetingId: roomInfo.Meeting.MeetingId,
+      })
+      .promise();
+  } catch (err) {
+    console.info('endMeeting > try/catch:', JSON.stringify(err, null, 2));
+  }
+
+  const result = await deleteGameRoom(roomId);
+  console.info('deleteMeeting > result:', JSON.stringify(result, null, 2));
+  return result;
+};
---
+++ game-room-service/lambda-functions/infrastructure/dynamo.repository.js
@@ -0,0 +1,43 @@
+const ddb = new AWS.DynamoDB();
+const { ROOM_TABLE_NAME } = process.env;
+
+// Retrieves the meeting from the table by the meeting title
+async function getGameRoom(roomId) {
+  const result = await ddb
+    .getItem({
+      TableName: ROOM_TABLE_NAME,
+      Key: {
+        roomId: {
+          S: roomId,
+        },
+      },
+    })
+    .promise();
+  return result.Item ? JSON.parse(result.Item.Data.S) : null;
+}
+
+// Stores the meeting in the table using the meeting title as the key
+async function putGameRoom(roomId, gameRoom) {
+  await ddb
+    .putItem({
+      TableName: ROOM_TABLE_NAME,
+      Item: {
+        roomId: { S: roomId },
+        roomData: { S: JSON.stringify(gameRoom) },
+      },
+    })
+    .promise();
+}
+
+async function deleteGameRoom(roomId) {
+  const result = await ddb
+    .delete({
+      TableName: ROOM_TABLE_NAME,
+      Item: {
+        roomId: { S: roomId },
+      },
+    })
+    .promise();
+  return result;
+}
+S;
